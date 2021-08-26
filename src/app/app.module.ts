266473dda110a2293fd29ed77b9e4e66db4878a5
sballew17 <samuel.ballew17@gmail.com>
2021-08-25 20:26:59 -0400

    Adding movie-card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,21 +8,34 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomePageComponent },
+  { path: 'movies', component: MovieCardComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     UserRegistrationFormComponent,
-    UserLoginFormComponent
+    UserLoginFormComponent,
+    MovieCardComponent,
+    WelcomePageComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
+    MatIconModule,
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -7,6 +7,7 @@ import { FetchAPiDataService } from '../fetch-api-data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 // Reference to a dialog opened via the MatDialog service.
 import { MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-login-form',
@@ -22,13 +23,15 @@ export class UserLoginFormComponent implements OnInit {
   constructor(
     public fetchApiData: FetchAPiDataService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar
+    public snackBar: MatSnackBar,
+    public router: Router
   ) { }
 
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   ngOnInit(): void { }
 
   loginUser(): void {
+    this.router.navigate(['movies']);
     this.fetchApiData.userLogin(this.userData).subscribe(
       // Login successful.
       (response) => {
@@ -40,12 +43,14 @@ export class UserLoginFormComponent implements OnInit {
         this.snackBar.open(`Welcome back, ${response.user.name}!`, 'OK', {
           duration: 3000,
         });
+
       },
       // Login unsuccessful.
       (response) => {
         this.snackBar.open(response, 'OK', {
           duration: 3000,
         });
+
       }
     );
   }
